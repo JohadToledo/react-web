fad4732f89f9dd97d0e63d0f0c3610d328149757
Johad Toledo <johadtledo@gmail.com>
2023-08-13 21:39:49 +1000

    change on styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import Header from "./components/Header/Header";
 import { Home } from "./components/Home/Home";
 import { Projects } from "./components/Projects/Projects";
