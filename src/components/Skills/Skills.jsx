c7dc3a82455eb7f3277a2df2ba12d9e5466aa837
Johad Toledo <johadtledo@gmail.com>
2023-08-14 21:07:10 +1000

    add some styles to skills component

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,16 +1,18 @@
 import { skills } from '../data'
+import './skill.css'
+
 export function Skills(){
     return(
         <section id="skill">
             <div className="container">
-                <div className="">
+                <div className="skill-text">
                     <h1>
                         Skill &amp; Technologies
                     </h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, veniam. Sint qui quaerat ratione nesciunt? Architecto vel est molestias molestiae. Quam, quae amet. Laboriosam, voluptatum illo. Voluptate ipsa numquam consequuntur?</p>
 
                 </div>
-                <div className="">
+                <div className="skills-items">
                     {skills.map((skills) => (
                 <div key={skills} className="skill-item">
                     <div className="item">
