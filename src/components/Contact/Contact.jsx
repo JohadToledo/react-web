ef6d7ff926d775b28b7a58259a7aa74c2ce738e2
Johad Toledo <johadtledo@gmail.com>
2023-08-15 20:03:23 +1000

    add contact section

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,52 @@
+import "./contact.css";
+
+export function Contact() {
+  return (
+    <section id="contact">
+         <div className="contact-header">
+        <h1>
+            <strong>Get in Touch</strong>
+        </h1>
+        </div>
+        <div className="form">
+        <form action="">
+        <div className="div-name">
+            <label id="name-label" htmlFor="name">
+            <input
+            placeholder="Name"
+            required
+            type="text"
+            id="name"
+            />
+            </label>
+        </div>
+        <div className="div-email">
+            <label id="email-label" htmlFor="email">
+            {" "}
+            <input 
+            className=""
+            placeholder="Email"
+            required
+            type="email"
+            id="email"
+            />
+            </label>
+        </div>
+        <div className="div-message">
+            <label id="message-label" htmlFor="message">
+            <textarea
+            rows={6}
+            cols={30} 
+            className=""
+            placeholder="message"
+            required
+            type="text"
+            id="Message"
+            />
+            </label>
+        </div>
+        </form>
+        </div>
+    </section>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import Header from "./components/Header/Header";
 import { Home } from "./components/Home/Home";
 import { Projects } from "./components/Projects/Projects";
 import { Skills } from "./components/Skills/Skills";
+import { Contact } from "./components/Contact/Contact";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Home />
     <Projects />
     <Skills />
+    <Contact />
       {/* <About />
       <Testimonials />
       <Contact />  */}
