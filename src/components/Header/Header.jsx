adafd117ea8a7d0fc4ac9de91da76c4fabb7fe16
Johad Toledo <johadtledo@gmail.com>
2023-08-16 20:13:41 +1000

    add some style to header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,16 +4,19 @@ import { Link } from 'react-scroll'
 
 function Header() {
   return (
-    <div className="header-content">
-      <div> Johad Toledo </div>
-      <nav>
-        <Link>Skills</Link>
-        <Link>Projects</Link>
-        <Link>About</Link>
-        <Link>Contacts</Link>
-      
-      </nav>
+    <section id='header'>
+      <div className="header-content">
+        <div> Johad Toledo </div>
+        <div className="navBar">
+          <nav>
+            <Link>Skills</Link>
+            <Link>Projects</Link>
+            <Link>About</Link>
+            <Link>Contacts</Link>
+          </nav>
+        </div>
     </div>
+    </section>
   );
 }
 
