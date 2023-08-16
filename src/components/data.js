e07ed3ff5109de44137851e5100d415e6196419f
Johad Toledo <johadtledo@gmail.com>
2023-08-16 22:05:07 +1000

    change some other info in data.js

--- src/components/data.js
+++ src/components/data.js
@@ -1,7 +1,6 @@
 export const projects = [
     {
       title: "Petra",
-      subtitle: "MERN Stack",
       description:
         " E-Commerce built in React.js that brings information from a Rest API and is divided into components, each component handles the information using React Hooks and Redux is used to share information between.",
       image: "src/assets/Petra.png",
@@ -9,7 +8,6 @@ export const projects = [
     },
     {
       title: "To Buy List",
-      subtitle: "React and Python",
       description:
         "This app is built with Vanilla JavaScript, HTML and Vanilla CSS. It's like a To-Do list app that permits making a list of different.",
       image: "src/assets/Tobuylist.png",
@@ -17,7 +15,6 @@ export const projects = [
     },
     {
       title: "DevChat",
-      subtitle: "React and Firebase",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
       image: "src/assets/IMG_6703.JPG",
@@ -25,7 +22,6 @@ export const projects = [
     },
     {
       title: "Epic Todo App",
-      subtitle: "React Hooks",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
       image: "src/assets/Elena.jpg",
