d19caf9617c4b8585e4582b209fee4700f736048
Johad Toledo <johadtledo@gmail.com>
2023-08-21 19:55:14 +1000

    add tailwind

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,10 +6,10 @@ import downloadImg from '../../assets/download.png'
 export function Home(){
     return(
         
-        <section id="home" className="">
+        <section id="home" className="w-screen max-w-7xl h-[calc(100vh-20px)] ">
             <div className="homeContent">
                 <span className="hello">Hi,</span>
-                <span className="homeText">I am <span className="homeName">Johad</span> <br />Web Developer</span>
+                <span className="homeText">I am <span className="text-indigo-600">Johad</span> <br />Web Developer</span>
                 <p className="homeParag">A Web developer building reactive webapps.</p>
                 <Link><button className="btn"><img className='btnImg'src={downloadImg} alt="download" /><span className="spanResume">Resume</span></button></Link>
             </div>
