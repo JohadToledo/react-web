f33d2ea4f7ed0709119af7ca1b8588a5a0685c37
Johad Toledo <johadtledo@gmail.com>
2023-08-25 21:41:08 +1000

    Change some style on Resume btn

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,15 +11,23 @@ export function Home(){
         link.target = '_blank'; // Open in a new tab if needed
         link.rel = 'noopener noreferrer';
         link.click();
-      }
-      
+        }
+
     return(
         <section id="home" className="w-screen max-w-7xl h-[calc(100vh-20px)] overflow-hidden flex flex-row-reverse items-end"> 
             <div className="h-screen w-screen p-8 leading-10 font-bold flex flex-col justify-center">
                 <span className="text-3xl font-thin">Hi,</span>
-                <span className="text-3xl font-thin">I am <span className="text-indigo-600">Johad</span> <br />Web Developer</span>
+                <span className="text-3xl font-thin">I am <span className="text-teal-600">Johad</span> <br />Web Developer</span>
                 <p className="text-3xl font-thin">A Web developer building reactive webapps.</p>
-                <Link to='https://www.linkedin.com/in/johad-toledo-webdeveloper/'><button id="downloadCV" onClick={downloadFileAtURL} value="download" className="bg-white text-3xl flex items-center rounded-3xl my-4 px-8"><img className='p-0 w-8 bg-transparent'src={downloadImg} alt="download" /><span className="bg-transparent pl-2">Resume</span></button></Link>
+                <Link to=''>
+                    <button id="downloadCV" 
+                    onClick={downloadFileAtURL} 
+                    value="download" 
+                    // className="bg-white  flex items-center rounded-3xl my-4 px-8 "
+                    className='my-4 border-3 rounded w-40 bg-teal-500 py-2 text-white text-lg flex justify-center items-center  hover:bg-teal-600'>
+                        <img className='p-0 w-8 bg-transparent'src={downloadImg} alt="download" />
+                        <span className="bg-transparent text-xl pl-2">Resume</span></button>
+                </Link>
             </div>
             <img src={reactsvg} className="absolute pr-12 h-96 bottom-64" alt="foto" />
         </section>
