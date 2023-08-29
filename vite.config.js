32b14611332eca6797b488b1d3204817605b289a
Johad Toledo <johadtledo@gmail.com>
2023-08-29 17:41:47 +1000

    path changed vite.conf

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        '@/assets/petra.png','@/assets/tobuylist.png','@/assets/troogle.png','@/assets/checklist.png' // Add other assets if needed
+      ],
+    },
+  },
 })
