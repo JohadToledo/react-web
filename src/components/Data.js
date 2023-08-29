a59f463ad1ee08e5d3c1d52ad180a71d4abaf78e
Johad Toledo <johadtledo@gmail.com>
2023-08-29 18:17:32 +1000

    path another time

--- src/components/Data.js
+++ src/components/Data.js
@@ -3,28 +3,28 @@ export const projects = [
       title: "Petra",
       description:
         " E-Commerce built in React.js that brings information from a Rest API and is divided into components, each component handles the information using React Hooks and Redux is used to share information between.",
-      image: '/assets/petra.png',
+      image: 'assets/petra.png',
       link: "https://codealo-commerce.vercel.app/",
     },
     {
       title: "To Buy List",
       description:
         "This app is built with Vanilla JavaScript, HTML and Vanilla CSS. It's like a To-Do list app that permits making a list of different.",
-      image: '/assets/tobuylist.png',
+      image: 'assets/tobuylist.png',
       link: "https://tobuylist.web.app/",
     },
     {
       title: "Google Clone",
       description:
         "Google clone with html and css",
-      image: '/assets/troogle.png',
+      image: 'assets/troogle.png',
       link: "https://troogle.web.app/",
     },
     {
       title: "Epic Todo App",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: '/assets/troogle.png',
+      image: 'assets/troogle.png',
       link: "https://pythonbootcamp.com",
     },
   ];
