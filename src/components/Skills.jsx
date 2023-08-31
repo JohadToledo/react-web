a3944be40c8b345a6f5d2bec43338d85c31c3c1c
Johad Toledo <johadtledo@gmail.com>
2023-08-31 18:42:09 +1000

    responsive design

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,7 +7,7 @@ export function Skills() {
   return (
       <section
         id="skills"
-        className="overflow-hidden w-screen max-w-7xl min-h-[80vh]"
+        className="overflow-hidden w-screen max-w-7xl h-auto"
       >
         <div className="">
           <div className="text-center">
@@ -17,11 +17,11 @@ export function Skills() {
             </p>
           </div>
           
-            <div className="py-4 gap-2.5 flex flex-wrap justify-center lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+            <div className="py-4 w-full gap-3 flex flex-wrap justify-center lg:w-4/5 sm:gap-2.5 sm:mx-auto sm:mb-2 -mx-2">
               {skills.map((skills) => (
                 <div
                   key={skills}
-                  className="border-solid border-teal-900 border-2 p-2.5"
+                  className="border-solid border-teal-900 border-2 p-2.5  mx-5 my-2"
                 >
                   <div className="text-white flex w-[20vw]">
                   <img className='mr-3 w-6 bg-transparent'src={Check} alt="checklist" />
