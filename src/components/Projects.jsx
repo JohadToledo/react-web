e49b1f8818f74933d1792d7f255cd8e12f5298c8
Johad Toledo <johadtledo@gmail.com>
2023-11-07 08:05:47 +1000

    add alternating card layout

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -55,7 +55,6 @@ export function Projects() {
             <div
               key={project.id}
               className="relative bg-cover bg-no-repeat bg-center z-10 h-[550px] w-full items-stretch justify-center py-0 sm:h-[700px] sm:w-[100%] md:h-[650px] md:w-[100%] xl:max-w-[1400px] lg:h-[500px] mb-10"
-              // className="flex flex-col items-center rounded-3xl m-5 sm:mx-10 relative"
             >
                   <img className={`absolute -bottom-0 w-[70%] sm:w-[85%] md:w-[60%] lg:max-w-[50%] ${
                     index % 2 === 0 ? 'left-0 rounded-bl-3xl' : 'right-0 rounded-br-3xl'
@@ -65,33 +64,33 @@ export function Projects() {
                           src={project.image}/
                           >
                   <div
-                  className={`absolute top-0 text-[#0E1016] ml-8 lg:ml-14 mt-6 flex  items-center justify-center gap-4 lg:mt-10 ${
-                    index % 2 === 0 ? 'right-0' : 'left-0'
+                  className={`absolute top-0 text-[#0E1016] mt-6 flex items-center justify-center gap-4 lg:mt-10 ${
+                    index % 2 === 0 ? 'right-0 lg:mr-14 mr-8' : 'left-0 ml-8 lg:ml-14'
                   }`}
-                  //  className=" h-16 flex justify-end p-5  bg-transparent mt-2 gap-1 rounded-3xl absolute top-0 left-0"
                   >
-                    <button>
+                    
                       <a href={project.gitLink} key={project.image} rel="noreferrer" target="_blank"
-                      className="rounded-[1rem] w-[30rem] h-96  opacity-70 hover:opacity-100 rounded-2xl"
+                      className="rounded-full w-[20px] bg-lorange p-5 text-[20px] md:w-[25px] md:text-[24px] lg:w-[65px] lg:text-[28px]"
+                      // className="rounded-[1rem] w-[30rem] h-96  opacity-70 hover:opacity-100 rounded-2xl"
                       >
                         <div className="stroke-red hover:stroke-lblue bg-transparent m-auto rounded-2xl">
                           <span dangerouslySetInnerHTML={{ __html: project.iconGit }}></span>
                         </div>
                       </a>
-                    </button>
-                    <button>
+                    
                       <a href={project.link} key={project.image} rel="noreferrer" target="_blank"
-                      className="rounded-[1rem] w-[30rem] h-96  opacity-70 hover:opacity-100"
+                       className="w-[20px] rounded-full bg-lorange p-5 text-[20px] md:w-[25px] md:text-[24px] lg:w-[65px] lg:text-[28px]"
+                      // className="rounded-[1rem] w-[30rem] h-96  opacity-70 hover:opacity-100"
                       >
                         <div className="stroke-red stroke-[0.5] hover:stroke-black bg-transparent opacity-100">
                           <span dangerouslySetInnerHTML={{ __html: project.iconLink }}></span>
                         </div>
                       </a>
-                    </button>
+                   
                   </div>
-                  <div  
-                  className={`absolute text-white ml-10 md:mr-12 lg:top-52 lg:ml-4 mb-10 md:mb-16 lg:mb-14 ${
-                    index % 2 === 0 ?  'right-0' :'left-0' 
+                  <div
+                  className={`absolute text-white top-32 ml-10 md:mr-12 lg:top-52 lg:ml-4 mb-10 md:mb-16 lg:mb-14 ${
+                    index % 2 === 0 ?  'right-0 mr-0 ml-10 md:right-0 md:ml-0 lg:right-10 lg:top-60  lg:mr-4 mb-10  md:mb-16 lg:mb-14' :'left-10 ml-0 md:mr-12 lg:top-52 lg:ml-4 mb-10  md:mb-16 lg:mb-14 ' 
                     }`}
                   // className="absolute text-white  left-10 top-32 ml-0 md:mr-12 lg:top-52 lg:ml-4 mb-10  md:mb-16 lg:mb-14 "
                   // className="bg-transparent opacity-100 absolute top-14 lg:flex-raw"
